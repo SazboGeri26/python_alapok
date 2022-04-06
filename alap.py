f587a9806892f372142782fe1cecfca069c4c64f
SazboGeri26 <gergoszabo701@gmail.com>
2022-04-06 10:23:05 +0200

    masodik kommit

--- alap.py
+++ alap.py
@@ -1,2 +1,5 @@
 from os import system
+from vezerloSzerkezet import eldont
 system('cls')
+
+eldont(12, 25)
---
+++ vezerloSzerkezetek.py
@@ -0,0 +1,7 @@
+# if , if-else, if-elif-else
+
+#metodus a számok közötti viszony eldöntésére
+def eldont(elso, masodik):
+    if elso < masodik:
+        print('Az {} nagyobb, mint {}'. format(masodik, nagyobb))
+        
